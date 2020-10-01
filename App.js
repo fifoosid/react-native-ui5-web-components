61140adbff343d589ee73a30db4baeb7e0cf8f3e
Filip Siderov <filip.siderov@sap.com>
2020-10-01 17:05:32 +0300

    Add ui5-webcomponents to the project

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import "@ui5/webcomponents/dist/DatePicker";
+import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents/dist/MessageStrip";
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+    <ui5-button>Hello World</ui5-button>
+      <ui5-datepicker></ui5-datepicker>
+      <ui5-messagestrip>All the best!</ui5-messagestrip>
     </View>
   );
 }
